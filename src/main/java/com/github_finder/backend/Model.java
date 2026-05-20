21fcc39b556a1cb54e2a36ea38b64ada4e431756
Purbasa Maharana <purbasamaharana@gmail.com>
2026-05-21 00:25:26 +0530

    Update Model.java

--- src/main/java/com/github_finder/backend/Model.java
+++ src/main/java/com/github_finder/backend/Model.java
@@ -1,24 +1,25 @@
 package com.github_finder.backend;
 
+import jakarta.persistence.*;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 public class Model {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
