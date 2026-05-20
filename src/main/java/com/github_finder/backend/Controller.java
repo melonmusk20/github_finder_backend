3378a10ca1c5a97342b47c778c7c4b61deef4943
melonmusk20 <purbasamaharana@gmail.com>
2026-05-20 21:13:01 +0530

    Initial commit

---
+++ src/main/java/com/github_finder/backend/Controller.java
@@ -0,0 +1,32 @@
+package com.github_finder.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import tools.jackson.databind.ObjectReader;
+
+@RestController
+public class Controller {
+
+    public Controller(Repository repo) {
+        this.repo = repo;
+    }
+
+    private final Repository repo;
+
+    @GetMapping("/{username}")
+    public Object getGithubProfile(@PathVariable String username){
+
+        String url = "https://api.github.com/users/" + username ;
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        Model history = new Model();
+        history.setUsername(username);
+
+        repo.save(history);
+
+        return restTemplate.getForObject(url, Object.class);
+    }
+}
---
+++ src/main/java/com/github_finder/backend/Repository.java
@@ -0,0 +1,6 @@
+package com.github_finder.backend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Repository extends JpaRepository<Model, Long> {
+}
